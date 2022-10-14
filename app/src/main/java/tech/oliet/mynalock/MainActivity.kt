e2650b25aaf03fa7e336b39a6ec87c86c0d46d51
OLIET2357 <oliet2357@gmail.com>
2022-10-14 22:28:23 +0900

    lock

--- app/src/main/java/tech/oliet/mynalock/MainActivity.kt
+++ app/src/main/java/tech/oliet/mynalock/MainActivity.kt
@@ -5,9 +5,13 @@ import android.nfc.Tag
 import android.nfc.tech.IsoDep
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+    private val handler = Handler(Looper.getMainLooper())
     private var nfcAdapter: NfcAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,11 +60,54 @@ class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
         val isoDep = IsoDep.get(tag)
         isoDep.connect()
 
-        repeat(10) {
-            val command = byteArrayOf()
-            val response = isoDep.transceive(command)
-        }
+        val result = lockmyna(isoDep)
 
         isoDep.close()
+
+        handler.post {
+            Toast.makeText(
+                this, if (result) {
+                    getString(R.string.succeeded)
+                } else {
+                    getString(R.string.failed)
+                }, Toast.LENGTH_LONG
+            ).show()
+        }
+
+    }
+
+    private fun lockmyna(isoDep: IsoDep): Boolean {
+        // SELECT FILE 券面事項入力補助AP
+        val command1 = byteArrayOf(
+            0x00, 0xA4.toByte(), 0x04, 0x0C,
+            0x0A, 0xD3.toByte(), 0x92.toByte(),
+            0x10, 0x00, 0x31, 0x00, 0x01,
+            0x01, 0x04, 0x08
+        )
+        val response1 = isoDep.transceive(command1)
+
+        if (!(response1[0] == 0x90.toByte() && response1[1] == 0x00.toByte())) {
+            return false
+        }
+
+        // SELECT FILE 暗証番号
+        val command2 = byteArrayOf(0x00, 0xA4.toByte(), 0x02, 0x0C, 0x02, 0x00, 0x11)
+        val response2 = isoDep.transceive(command2)
+
+        if (!(response2[0] == 0x90.toByte() && response2[1] == 0x00.toByte())) {
+            return false
+        }
+
+        repeat(10) {
+            // VERIFY
+            val command3 =
+                byteArrayOf(0x00, 0x20, 0x00, 0x80.toByte(), 0x04, 0x4c, 0x4f, 0x43, 0x4b)
+            val response3 = isoDep.transceive(command3)
+            if (response3[0] == 0x69.toByte() && response3[1] == 0x84.toByte()) {
+                return true
+            }
+        }
+
+        return false
     }
 }
