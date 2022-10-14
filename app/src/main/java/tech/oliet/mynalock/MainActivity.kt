f06346134574d3e86c5b2de9b8dee1fb5bc59495
OLIET2357 <oliet2357@gmail.com>
2022-10-14 17:45:40 +0900

    make surface

--- app/src/main/java/tech/oliet/mynalock/MainActivity.kt
+++ app/src/main/java/tech/oliet/mynalock/MainActivity.kt
@@ -1,11 +1,64 @@
 package tech.oliet.mynalock
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+    private var nfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (nfcAdapter == null) {
+            AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(getString(R.string.no_nfc))
+                .show()
+            return
+        }
+
+        if (nfcAdapter?.isEnabled == false) {
+            AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(getString(R.string.nfc_is_off))
+                .show()
+            return
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(
+            this,
+            this,
+            NfcAdapter.FLAG_READER_NFC_B or NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK, ,
+            null
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        tag ?: return
+        val isoDep = IsoDep.get(tag)
+        isoDep.connect()
+
+        repeat(10) {
+            val command = byteArrayOf()
+            val response = isoDep.transceive(command)
+        }
+
+        isoDep.close()
     }
 }
